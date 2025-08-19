0dc4d669c74ea74e77daaf19b4c118c07d568c97
Ashley Domineck <adouglas0721@gmail.com>
2025-08-18 22:39:13 -0400

    js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 #  SBA_316
-The Document Object Model
+## The Document Object Model
+
+## Reflection
+## What could you have done differently during the planning stages of your project to make the execution easier?
+## Were there any requirements that were difficult to implement? What do you think would make them easier to implement in future projects?
+ 
+ 
+## What would you add to, or change about your application if given more time?
+
+## Use this space to make notes for your future self about anything that you think is important to remember about this process, or that may aid you when attempting something similar again.
---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+
+
+const quoteText = document.getElementById('quote-text');
+const quoteAuthor = document.getElementById('quote-author');
+const newQuoteBtn = document.querySelector('#new-quote');
+
+
+const quotes =[
+    {text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius inventore optio voluptatibus nobis ut culpa in architecto. Itaque, dolores ullam! Beatae porro, reprehenderit corporis nisi accusantium saepe sapiente dicta voluptate?", author: "Someone"},
+    {text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas placeat quae provident totam debitis amet ex labore aspernatur ipsum dolore eaque facere quidem reprehenderit, ratione ipsam culpa corporis quam ullam!", author: "Something"},
+    {text:"Moew Moew", author: "A Cat"},
+    {text:"Bark Bark, Ruff Ruff", author: "A Dog"},
+    {text:"Happy Monday!For all those wanting more practice and understanding of JS! This weeks lesson is for you!! ", author: "Dylan"},
+    {text:":partyparrot: Office hours will start at 5pm. Join Here at 5pm", author: "Constance"},
+    {text:"Life is a journey, not a destination.", author: "A person"},
+    {text:"I love blue cheese!", author: "Noone"}
+];
+
+newQuoteBtn.addEventListener('click', function() {
+
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+
+    const selected = quotes[randomIndex];
+
+    quoteText.textContent = selected.text;
+    quoteAuthor.textContent = `- ${selected.author}`;
+
+});
